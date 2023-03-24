9ff34a4469d27bbff6a82365dcc0b0958b2c88e6
Mark <sandmdelgados@gmail.com>
2023-03-24 17:11:09 -0500

    Chapter 3 completed

--- private/functions.php
+++ private/functions.php
@@ -9,3 +9,18 @@
         return WWW_ROOT . $scriptPath;
     }
 
+    function error_404(){
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 NOT FOUND");
+        exit();
+    }
+
+    function error_500(){
+        header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+        exit();
+    }
+
+    function redirectTo($location){
+        header('Location: ' . url_for($location));
+        exit;
+    }
+
